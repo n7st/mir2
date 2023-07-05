d7c4c1ca8888dc4a035cf08f6067c61bf8df8b63
Mike Jones <mike@netsplit.org.uk>
2023-07-05 23:00:57 +0100

    Daemon boilerplate

---
+++ Server.Daemon/MirConfig.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace Server.Daemon
+{
+    public class MirConfig
+    {
+        public string DaemonName { get; set; }
+    }
+}
--- Server.Daemon/Program.cs
+++ Server.Daemon/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Threading.Tasks;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Server.Daemon
+{
+    /// <summary>
+    /// <c>Program</c> is the daemon's entrypoint. Running it launches the application.
+    /// </summary>
+    class Program
+    {
+        public static async Task Main(string []args)
+        {
+            var builder = new HostBuilder()
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    config.AddEnvironmentVariables();
+
+                    if (args != null) config.AddCommandLine(args);
+                })
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddOptions();
+                    services.Configure<MirConfig>(hostContext.Configuration.GetSection("Daemon"));
+                    services.AddSingleton<IHostedService, MirService>();
+                })
+                .ConfigureLogging((hostingContext, logging) =>
+                {
+                    logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                    logging.AddConsole();
+                });
+
+            await builder.RunConsoleAsync();
+        }
+    }
+}
